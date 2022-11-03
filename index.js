ebfaab7c3cfee62f0e1c4bc181927d6b1f0e7f8e
JojoDuke <jojoamankwa@gmail.com>
2022-11-03 21:05:10 +0000

    scraping

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+// MODULES
 const rwClient = require("./TwitterClient.js");
 const cronjob = require("cron").CronJob;
+const puppeterr = require("puppeteer");
+
+(async () => {
+    const chromeBrowser = await puppeterr.launch({ headless: false });
+    const page = await chromeBrowser.newPage();
+    await page.goto("https://www.sec.gov/edgar/search/#/category=form-cat2");
+
+    
+})();
 
 const tweet = async () => {
     try {
