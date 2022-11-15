e5bec7c4851db7da208cc0964eb665114f003a72
JojoDuke <jojoamankwa@gmail.com>
2022-11-15 12:42:38 +0000

    final

--- index.js
+++ index.js
@@ -27,32 +27,13 @@ const numShares = require("./numShares.js");
         }
     }
 
-    const job = new cronjob("* * * * *", async () => {
-        let price = priceModule();
-        if (price === null) {
-            console.log("Not today");
-        } else {
-            console.log(
-                "New insider trade! (form 4 filed)" + '\n' + '\n' +
-                await nameModule() + " bought " + await numShares() + " shares at " + "$" + await priceModule() + '\n' + '\n' +
-        
-                "Amount Purchased: " + "$" + await numShares() * await priceModule() + '\n' +
-                "Stock: " +  await stockModule() + '\n' +
-                "Date: " + await dateModule() + '\n'
-            );
-        }
-        
-    });
-
-    job.start();
-
     // CronJob, starts from 10 am to 8pm EST
-    /*const job = new cronjob("0 13-21 * * *", () => {
+    const job = new cronjob("0 13-21 * * *", () => {
         tweet();
         console.log("Tweet executed");
     });
 
-    job.start();*/
+    job.start();
   
 })();
 
